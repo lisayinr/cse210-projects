b2b0087c8570a35a127336ec81d6378c2ae607c0
lisayinr <lisayin@byui.edu>
2024-06-19 12:52:42 -0600

    fixed issue where all assignments were on one program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,101);
+        int guess = -1;
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! ");
+            }
+        }
+
     }
 }
