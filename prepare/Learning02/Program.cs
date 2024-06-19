498deb8a45daa8176489cf5cc1f06d0edac99526
lisayinr <lisayin@byui.edu>
2024-06-19 12:21:54 -0600

    Completed w02 Learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2016;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Assistant";
+        job2._company = "Microsoft";
+        job2._startYear = 2005;
+        job2._endYear = 2020;
+
+        Resume theResume = new Resume();
+        theResume._name = "Lillis Green";
+        theResume._jobs.Add(job1);
+        theResume._jobs.Add(job2);
+        theResume.DisplayResume();
     }
 }
